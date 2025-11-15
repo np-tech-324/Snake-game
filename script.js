dfbf2afe26df0b4bc27166b74e4920a6653bb976
np-tech-324 <nishantpoojari324@gmail.com>
2025-11-16 01:29:37 +0530

    snake is ready

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const board = document.querySelector('.board')
 
-const blockHeight = 30
-const blockwidth = 30
+const blockHeight = 50
+const blockwidth = 50
 
 const rows = Math.floor(board.clientHeight / blockHeight)
 const cols = Math.floor(board.clientWidth / blockwidth)
+
+const blocks = [];
+const snake = [
+    {
+        x: 1,
+        y: 3
+    },
+    {
+        x: 1,
+        y: 4
+    },
+    {
+        x: 1,
+        y: 5
+    },
+
+]
 
 // for(let i = 0; i < rows*cols;i++){
 //     let block = document.createElement('div')
@@ -14,10 +31,21 @@ const cols = Math.floor(board.clientWidth / blockwidth)
 
 for(let row = 0;row < rows; row++){
     for(let col = 0; col < cols; col++){
-        let block = document.createElement('div')
+        const block = document.createElement('div')
         block.classList.add("block")
-        board.appendChild(block)
-        
-        // block.innerHTML = `${row}-${col}`
+        board.appendChild(block);
+        block.innerHTML = `${row}-${col}`
+        blocks[`${row}-${col}`] = block;
+        // console.log(block);
     }
 }
+
+function render(){
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
+
+setInterval(() => {
+    render()
+}, 300);
