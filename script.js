15e98ba134ad3db964100ec808b6ff0eec7d9309
np-tech-324 <nishantpoojari324@gmail.com>
2025-11-15 23:35:51 +0530

    board has co-ordinate sucessfully

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ const blockwidth = 30
 const rows = Math.floor(board.clientHeight / blockHeight)
 const cols = Math.floor(board.clientWidth / blockwidth)
 
-for(let i = 0; i < rows*cols;i++){
-    let block = document.createElement('div')
-    block.classList.add("block")
-    board.appendChild(block)
+// for(let i = 0; i < rows*cols;i++){
+//     let block = document.createElement('div')
+//     block.classList.add("block")
+//     board.appendChild(block)
+// }
+
+for(let row = 0;row < rows; row++){
+    for(let col = 0; col < cols; col++){
+        let block = document.createElement('div')
+        block.classList.add("block")
+        board.appendChild(block)
+        
+        // block.innerHTML = `${row}-${col}`
+    }
 }
